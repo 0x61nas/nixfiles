71352cc958d21aeaa89927dda33c9e6a9f6e1116
0x61nas <anas.elgarhy.dev@gmail.com>
2024-05-06 10:45:15 +0300

    feat(rofi): add plugins, and use gruvbox

--- configs/rofi.nix
+++ configs/rofi.nix
@@ -3,9 +3,9 @@
   programs.rofi = {
     enable = true;
     plugins = with pkgs; [ rofi-calc rofi-emoji ];
-    #theme = "gruvbox-dark";
+    theme = "gruvbox-dark";
     extraConfig = {
-      modi = "drun,emoji,ssh";
+      modi = "drun,calc,emoji,ssh";
       kb-primary-paste = "Control+V,Shift+Insert";
       kb-secondary-paste = "Control+v,Insert";
     };
