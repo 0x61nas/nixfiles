fb9384b329b89c3335201a133d3fc5dde0b5a5a4
0x61nas <anas.elgarhy.dev@gmail.com>
2024-05-09 03:19:16 +0300

    feat(telescope): fix the keys

--- configs/nvim/nvim/lua/plugins/telescope.lua
+++ configs/nvim/nvim/lua/plugins/telescope.lua
@@ -4,13 +4,13 @@ return {
     keys = {
       {'<Leader>f', ":lua require('telescope.builtin').find_files()<CR>", {noremap = true, silent = true}, desc="Find Files"},
       {'<Leader>b', ":lua require('telescope.builtin').buffers()<CR>", {noremap = true, silent = true}, desc="Buffers"},
-      {'<Leader>fg', ":lua require('telescope.builtin').live_grep()<CR>", {noremap = true, silent = true}, desc="Live Grep"},
-      {'<Leader>fh', ":lua require('telescope.builtin').help_tags()<CR>", {noremap = true, silent = true}, desc="Help Tags"},
+      {'<Leader>g', ":lua require('telescope.builtin').live_grep()<CR>", {noremap = true, silent = true}, desc="Live Grep"},
+      {'<Leader>h', ":lua require('telescope.builtin').help_tags()<CR>", {noremap = true, silent = true}, desc="Help Tags"},
       {'<Leader>a', ":lua require('telescope.builtin').spell_suggest()<CR>", {noremap = true, silent = true}, desc="Spell Suggest"},
-      {'<Leader>fk', ":lua require('telescope.builtin').keymaps()<CR>", {noremap = true, silent = true}, desc="Show Key Maps"},
-      {'<Leader>fvo', ":lua require('telescope.builtin').vim_options()<CR>", {noremap = true, silent = true}, desc="Neovim Options"},
-      {'<Leader>fd', ":lua require('telescope.builtin').diagnostics()<CR>", {noremap = true, silent = true}, desc="Diagnostics"},
-      {'<Leader>fcs', ":lua require('telescope.builtin').git_status()<CR>", {noremap = true, silent = true}, desc="Git Status"},
+      {'<Leader>k', ":lua require('telescope.builtin').keymaps()<CR>", {noremap = true, silent = true}, desc="Show Key Maps"},
+      {'<Leader>vo', ":lua require('telescope.builtin').vim_options()<CR>", {noremap = true, silent = true}, desc="Neovim Options"},
+      {'<Leader>D', ":lua require('telescope.builtin').diagnostics()<CR>", {noremap = true, silent = true}, desc="Diagnostics"},
+      {'<Leader>cs', ":lua require('telescope.builtin').git_status()<CR>", {noremap = true, silent = true}, desc="Git Status"},
     },
     dependencies = {
         {'nvim-lua/plenary.nvim'}, {'nvim-telescope/telescope-fzf-native.nvim'}
