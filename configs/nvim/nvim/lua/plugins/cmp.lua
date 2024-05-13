3dada4bd3f64637c8657bc484f66bd5254f093b6
0x61nas <anas.elgarhy.dev@gmail.com>
2024-05-13 07:57:46 +0300

    fix(nvim/cmp): fix the key mapping

--- configs/nvim/nvim/lua/plugins/cmp.lua
+++ configs/nvim/nvim/lua/plugins/cmp.lua
@@ -16,10 +16,22 @@ return {
         end,
       },
       mapping = cmp.mapping.preset.insert({
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
       ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
+      ['<C-;>'] = cmp.mapping.abort(),
       ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
     sources = cmp.config.sources({
