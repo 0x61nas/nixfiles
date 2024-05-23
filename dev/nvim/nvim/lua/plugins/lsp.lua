a0f5365acc84b36ce68963dc08f7e9737bfadd8f
0x61nas <anas.elgarhy.dev@gmail.com>
2024-05-23 17:17:56 +0300

    fix(dev/nvim/lsp): rename

--- dev/nvim/nvim/lua/plugins/lsp.lua
+++ dev/nvim/nvim/lua/plugins/lsp.lua
@@ -7,6 +7,7 @@ return {
     { 'gi',         vim.lsp.buf.implementation,  { noremap = true, silent = true }, desc = "Go To Implementation", mode = 'n' },
     { "K",          vim.lsp.buf.hover,           { silent = true },                 desc = "Show Info",            mode = "n" },
     { "<leader>k",  vim.lsp.buf.signature_help,  { silent = true, noremap = true }, desc = "Show Signature",       mode = "n" },
+    { "<leader>r",  vim.lsp.buf.rename,          { silent = true, noremap = true }, desc = "Rename symbol",       mode = "n" },
     { "<leader>F",  vim.lsp.buf.format,          { silent = true, noremap = true }, desc = "Format",               mode = "n" },
     { '<Leader>gr', vim.lsp.buf.references,      { noremap = true, silent = true }, desc = "Go To References",     mode = 'n' },
     { '<Leader>D',  vim.lsp.buf.type_definition, { noremap = true, silent = true }, desc = "Show Type Definition", mode = 'n' },
