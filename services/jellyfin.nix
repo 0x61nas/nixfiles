215f4be7f049fd536727efd14ace3910cf4c15b2
0x61nas <anas.elgarhy.dev@gmail.com>
2024-05-30 19:46:34 +0300

    feat(services/jellyfin): use the unstabe channel

--- services/jellyfin.nix
+++ services/jellyfin.nix
@@ -1,13 +1,9 @@
-{ pkgs, ... }: {
+{ pkgs-unstable, ... }: {
   users.groups.media = { };
   services.jellyfin = {
     enable = true;
     group = "media";
+    package = pkgs-unstable.jellyfin;
     # dataDir = "/mnt/data/media/jellyfin/var"
   };
-
-  environment.systemPackages = with pkgs; [
-    # jellyfin-web
-    #jellyfin-ffmpeg
-  ];
 }
