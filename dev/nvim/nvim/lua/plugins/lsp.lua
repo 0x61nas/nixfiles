53582cd3b8c2c8490348471bf148144b608d93d7
0x61nas <anas.elgarhy.dev@gmail.com>
2024-06-27 10:42:39 +0300

    feat(nvim): use the lsp for rust

--- dev/nvim/nvim/lua/plugins/lsp.lua
+++ dev/nvim/nvim/lua/plugins/lsp.lua
@@ -1,16 +1,20 @@
 return {
   'neovim/nvim-lspconfig',
+  opts = {
+    inlay_hints = { enable = true, },
+  },
   lazy = false,
   keys = {
     { 'gD',         vim.lsp.buf.declaration,     { noremap = true, silent = true }, desc = "Go To Declaration",    mode = 'n' },
     { 'gd',         vim.lsp.buf.definition,      { noremap = true, silent = true }, desc = "Go To Definition",     mode = 'n' },
     { 'gi',         vim.lsp.buf.implementation,  { noremap = true, silent = true }, desc = "Go To Implementation", mode = 'n' },
     { "K",          vim.lsp.buf.hover,           { silent = true },                 desc = "Show Info",            mode = "n" },
-    { "<leader>k",  vim.lsp.buf.signature_help,  { silent = true, noremap = true }, desc = "Show Signature",       mode = "n" },
+    { "<leader>ck",  vim.lsp.buf.signature_help,  { silent = true, noremap = true }, desc = "Show Signature",       mode = "n" },
     { "<leader>r",  vim.lsp.buf.rename,          { silent = true, noremap = true }, desc = "Rename symbol",       mode = "n" },
-    { "<leader>F",  vim.lsp.buf.format,          { silent = true, noremap = true }, desc = "Format",               mode = "n" },
+    { "<leader>cf",  vim.lsp.buf.format,          { silent = true, noremap = true }, desc = "Format",               mode = "n" },
     { '<Leader>gr', vim.lsp.buf.references,      { noremap = true, silent = true }, desc = "Go To References",     mode = 'n' },
     { '<Leader>D',  vim.lsp.buf.type_definition, { noremap = true, silent = true }, desc = "Show Type Definition", mode = 'n' },
+    { '<Leader>ca', vim.lsp.buf.code_action,     { noremap = true, silent = true }, desc = "Code actions" },
   },
   config = function()
     -- Setup language servers.
@@ -19,29 +23,51 @@ return {
     capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
     -- Rust
-    -- lspconfig.rust_analyzer.setup {
-    --   -- Server-specific settings. See `:help lspconfig-setup`
-    --   capabilities = capabilities,
-    --   -- on_attach = on_attach,
-    --   autostart = true,
-    --   settings = {
-    --     ["rust-analyzer"] = {
-    --       cargo = {
-    --         allFeatures = true,
-    --       },
-    --       imports = {
-    --         group = {
-    --           enable = false,
-    --         },
-    --       },
-    --       completion = {
-    --         postfix = {
-    --           enable = false,
-    --         },
-    --       },
-    --     },
-    --   },
-    -- }
+    lspconfig.rust_analyzer.setup {
+      -- Server-specific settings. See `:help lspconfig-setup`
+      capabilities = capabilities,
+      -- on_attach = on_attach,
+      autostart = true,
+      settings = {
+        ["rust-analyzer"] = {
+          cargo = {
+            allFeatures = true,
+            loadOutDirsFromCheck = true,
+            buildScripts = {
+              enable = true,
+            },
+          },
+          assist = {
+            -- Whether to insert #[must_use] when generating as_ methods for enum variants.
+            emitMustUse = true,
+          },
+          imports = {
+            group = {
+              enable = false,
+            },
+          },
+          completion = {
+            postfix = {
+              enable = false,
+            },
+          },
+          -- Add clippy lints for Rust.
+          checkOnSave = {
+            allFeatures = true,
+            command = "clippy",
+            extraArgs = { "--no-deps" },
+          },
+          procMacro = {
+            enable = true,
+            ignored = {
+              ["async-trait"] = { "async_trait" },
+              ["napi-derive"] = { "napi" },
+              ["async-recursion"] = { "async_recursion" },
+            },
+          },
+        },
+      },
+    }
 
     -- nix
     lspconfig.nil_ls.setup {
@@ -66,7 +92,8 @@ return {
         Lua = {
           diagnostics = {
             globals = { "vim" }
-          }
+          },
+          hint = { enable = true },
         }
       }
     }
