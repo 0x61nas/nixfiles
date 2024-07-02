43cd4eb4df7a63e141892cc702180277213fbb14
0x61nas <anas.elgarhy.dev@gmail.com>
2024-07-02 14:23:34 +0300

    feat(tlp): use the power policy when we on BAT

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -114,7 +114,10 @@
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
       CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+      PLATFORM_PROFILE_ON_AC = "performance";
+      PLATFORM_PROFILE_ON_BAT = "low-power";
 
       CPU_HWP_DYN_BOOST_ON_AC = 1;
       CPU_HWP_DYN_BOOST_ON_BAT = 0;
@@ -122,10 +125,10 @@
       CPU_MIN_PERF_ON_AC = 0;
       CPU_MAX_PERF_ON_AC = 100;
       CPU_MIN_PERF_ON_BAT = 0;
-      CPU_MAX_PERF_ON_BAT = 65;
+      CPU_MAX_PERF_ON_BAT = 90;
 
       #Optional helps save long term battery health
-      START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
+      START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
       STOP_CHARGE_THRESH_BAT0 = 1;
       RESTORE_THRESHOLDS_ON_BAT = 1;
     };
