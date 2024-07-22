c59c8dc2ee61da5f5fad4eb00f7be109121080a6
0x61nas <anas.elgarhy.dev@gmail.com>
2024-07-22 12:11:50 +0300

    feat(dev): add more LSPs and linters

--- dev/nvim/default.nix
+++ dev/nvim/default.nix
@@ -1,13 +1,18 @@
-{ pkgs, ... }:
+{ pkgs-unstable, ... }:
 {
-  home.packages = with pkgs; [
+  home.packages = with pkgs-unstable; [
     lua-language-server
     nil
-    nixpkgs-fmt
     rust-analyzer
     stylua
     nodePackages.bash-language-server
     asm-lsp
+    # nodePackages_latest.lua-fmt
+    luaformatter
+    python312Packages.python-lsp-server
+    gopls
+    zls
+    pylint
     # nasmfmt
     # typos
 
