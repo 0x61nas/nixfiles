da3ec8637f07b75b0ab6bae70f44313c80d4f95e
0x61nas <anas.elgarhy.dev@gmail.com>
2024-07-22 12:19:57 +0300

    feat(nvim/lsp): setup more LSPs and enable inlay hints

--- dev/nvim/nvim/lua/plugins/lsp.lua
+++ dev/nvim/nvim/lua/plugins/lsp.lua
@@ -1,130 +1,224 @@
 return {
-  'neovim/nvim-lspconfig',
-  opts = {
-    inlay_hints = { enable = true, },
-  },
-  lazy = false,
-  keys = {
-    { 'gD',         vim.lsp.buf.declaration,     { noremap = true, silent = true }, desc = "Go To Declaration",    mode = 'n' },
-    { 'gd',         vim.lsp.buf.definition,      { noremap = true, silent = true }, desc = "Go To Definition",     mode = 'n' },
-    { 'gi',         vim.lsp.buf.implementation,  { noremap = true, silent = true }, desc = "Go To Implementation", mode = 'n' },
-    { "K",          vim.lsp.buf.hover,           { silent = true },                 desc = "Show Info",            mode = "n" },
-    { "<leader>ck",  vim.lsp.buf.signature_help,  { silent = true, noremap = true }, desc = "Show Signature",       mode = "n" },
-    { "<leader>r",  vim.lsp.buf.rename,          { silent = true, noremap = true }, desc = "Rename symbol",       mode = "n" },
-    { "<leader>cf",  vim.lsp.buf.format,          { silent = true, noremap = true }, desc = "Format",               mode = "n" },
-    { '<Leader>gr', vim.lsp.buf.references,      { noremap = true, silent = true }, desc = "Go To References",     mode = 'n' },
-    { '<Leader>D',  vim.lsp.buf.type_definition, { noremap = true, silent = true }, desc = "Show Type Definition", mode = 'n' },
-    { '<Leader>ca', vim.lsp.buf.code_action,     { noremap = true, silent = true }, desc = "Code actions" },
-  },
-  config = function()
-    -- Setup language servers.
-    local lspconfig = require('lspconfig')
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
-    -- Rust
-    lspconfig.rust_analyzer.setup {
-      -- Server-specific settings. See `:help lspconfig-setup`
-      capabilities = capabilities,
-      -- on_attach = on_attach,
-      autostart = true,
-      settings = {
-        ["rust-analyzer"] = {
-          cargo = {
-            allFeatures = true,
-            loadOutDirsFromCheck = true,
-            buildScripts = {
-              enable = true,
-            },
-          },
-          assist = {
-            -- Whether to insert #[must_use] when generating as_ methods for enum variants.
-            emitMustUse = true,
-          },
-          imports = {
-            group = {
-              enable = false,
-            },
-          },
-          completion = {
-            postfix = {
-              enable = false,
-            },
-          },
-          -- Add clippy lints for Rust.
-          checkOnSave = {
-            allFeatures = true,
-            command = "clippy",
-            extraArgs = { "--no-deps" },
-          },
-          procMacro = {
-            enable = true,
-            ignored = {
-              ["async-trait"] = { "async_trait" },
-              ["napi-derive"] = { "napi" },
-              ["async-recursion"] = { "async_recursion" },
-            },
-          },
+    "neovim/nvim-lspconfig",
+    opts = {inlay_hints = {enable = true}},
+    lazy = false,
+    keys = {
+        {
+            "gD",
+            vim.lsp.buf.declaration,
+            {noremap = true, silent = true},
+            desc = "Go To Declaration",
+            mode = "n"
+        }, {
+            "gd",
+            vim.lsp.buf.definition,
+            {noremap = true, silent = true},
+            desc = "Go To Definition",
+            mode = "n"
+        }, {
+            "gi",
+            vim.lsp.buf.implementation,
+            {noremap = true, silent = true},
+            desc = "Go To Implementation",
+            mode = "n"
         },
-      },
-    }
-
-    -- nix
-    lspconfig.nil_ls.setup {
-      -- on_attach = on_attach,
-      capabilities = capabilities,
-      autostart = true,
-      settings = {
-        ["nil"] = {
-          formatting = {
-            command = { "nixpkgs-fmt" },
-          }
+        {
+            "K",
+            vim.lsp.buf.hover,
+            {silent = true},
+            desc = "Show Info",
+            mode = "n"
+        }, {
+            "<leader>ck",
+            vim.lsp.buf.signature_help,
+            {silent = true, noremap = true},
+            desc = "Show Signature",
+            mode = "n"
+        }, {
+            "<leader>r",
+            vim.lsp.buf.rename,
+            {silent = true, noremap = true},
+            desc = "Rename symbol",
+            mode = "n"
+        }, {
+            "<leader>cf",
+            vim.lsp.buf.format,
+            {silent = true, noremap = true},
+            desc = "Format",
+            mode = "n"
+        }, {
+            "<Leader>gr",
+            vim.lsp.buf.references,
+            {noremap = true, silent = true},
+            desc = "Go To References",
+            mode = "n"
+        }, {
+            "<Leader>D",
+            vim.lsp.buf.type_definition,
+            {noremap = true, silent = true},
+            desc = "Show Type Definition",
+            mode = "n"
+        }, {
+            "<Leader>ca",
+            vim.lsp.buf.code_action,
+            {noremap = true, silent = true},
+            desc = "Code actions"
         }
-      }
-    }
+    },
+    config = function()
+        -- Setup language servers.
+        local lspconfig = require("lspconfig")
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities =
+            require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-    -- lua
-    lspconfig.lua_ls.setup {
-      -- on_attach = on_attach,
-      capabilities = capabilities,
-      autostart = true,
-      settings = {
-        Lua = {
-          diagnostics = {
-            globals = { "vim" }
-          },
-          hint = { enable = true },
-        }
-      }
-    }
-
-    -- C/C++
-    lspconfig.clangd.setup {
-      -- on_attach = on_attach,
-      capabilities = capabilities,
-      autostart = true,
-    }
-
-    -- Bash LSP
-    local configs = require 'lspconfig.configs'
-    if not configs.bash_lsp and vim.fn.executable('bash-language-server') == 1 then
-      configs.bash_lsp = {
-        capabilities = capabilities,
-        autostart = true,
-        default_config = {
-          cmd = { 'bash-language-server', 'start' },
-          filetypes = { 'sh' },
-          root_dir = require('lspconfig').util.find_git_ancestor,
-          init_options = {
+        -- Rust
+        lspconfig.rust_analyzer.setup {
+            -- Server-specific settings. See `:help lspconfig-setup`
+            capabilities = capabilities,
+            -- on_attach = on_attach,
+            autostart = true,
             settings = {
-              args = {}
+                ["rust-analyzer"] = {
+                    cargo = {
+                        allFeatures = true,
+                        loadOutDirsFromCheck = true,
+                        buildScripts = {enable = true}
+                    },
+                    assist = {
+                        -- Whether to insert #[must_use] when generating as_ methods for enum variants.
+                        emitMustUse = true
+                    },
+                    imports = {group = {enable = false}},
+                    completion = {postfix = {enable = false}},
+                    -- Add clippy lints for Rust.
+                    checkOnSave = {
+                        allFeatures = true,
+                        command = "clippy",
+                        extraArgs = {"--no-deps"}
+                    },
+                    procMacro = {
+                        enable = true,
+                        ignored = {
+                            ["async-trait"] = {"async_trait"},
+                            ["napi-derive"] = {"napi"},
+                            ["async-recursion"] = {"async_recursion"}
+                        }
+                    },
+                    hint = {enable = true}
+                }
             }
-          }
         }
-      }
+
+        -- nix
+        lspconfig.nil_ls.setup {
+            -- on_attach = on_attach,
+            capabilities = capabilities,
+            autostart = true,
+            settings = {["nil"] = {formatting = {command = {"nixpkgs-fmt"}}}}
+        }
+
+        -- lua
+        lspconfig.lua_ls.setup {
+            -- on_attach = on_attach,
+            capabilities = capabilities,
+            autostart = true,
+            settings = {
+                Lua = {
+                    runtime = {version = "LuaJIT"},
+                    completion = {callSnippet = "Replace"},
+                    diagnostics = {globals = {"vim"}},
+                    format = {enable = true},
+                    hint = {enable = true}
+                }
+            }
+        }
+
+        lspconfig.gopls.setup {
+            capabilities = capabilities,
+            autostart = true,
+            cmd = {"gopls"},
+            filetypes = {"go", "gomod", "gowork", "gotmpl"},
+            single_file_support = true
+        }
+
+        lspconfig.pylsp.setup {
+            capabilities = capabilities,
+            autostart = true,
+            cmd = {"pylsp"},
+            filetypes = {"python"},
+            single_file_support = true,
+            settings = {
+                ["pylsp"] = {
+                    hint = {enable = true},
+                    plugins = {pylint = {enable = true}}
+                }
+            }
+        }
+
+        lspconfig.tsserver.setup {
+            capabilities = capabilities,
+            autostart = true,
+            cmd = {"typescript-language-server", "--stdio"},
+            filetypes = {
+                "javascript", "javascriptreact", "javascript.jsx", "typescript",
+                "typescriptreact", "typescript.tsx"
+            },
+            init_options = {hostInfo = "neovim"},
+            single_file_support = true,
+        }
+
+        lspconfig.gleam.setup {
+            capabilities = capabilities,
+            autostart = true,
+            cmd = {"gleam", "lsp"},
+            filetypes = {"gleam"},
+            root_dir = lspconfig.util.root_pattern("gleam.toml", ".git")
+        }
+
+        lspconfig.nil_ls.setup {
+            capabilities = capabilities,
+            autostart = true,
+            cmd = {"nil"},
+            filetypes = {"nix"},
+            root_pattern = {"flake.nix", ".git"},
+            single_file_support = true
+        }
+
+        lspconfig.zls.setup {
+            capabilities = capabilities,
+            autostart = true,
+            cmd = {"zls"},
+            filetypes = {"zig", "zir"},
+            single_file_support = true
+        }
+
+        -- C/C++
+        lspconfig.clangd.setup {
+            -- on_attach = on_attach,
+            capabilities = capabilities,
+            autostart = true,
+            single_file_support = true
+        }
+
+        -- Bash LSP
+        local configs = require "lspconfig.configs"
+        if not configs.bash_lsp and vim.fn.executable("bash-language-server") ==
+            1 then
+            configs.bash_lsp = {
+                capabilities = capabilities,
+                autostart = true,
+                default_config = {
+                    cmd = {"bash-language-server", "start"},
+                    filetypes = {"sh"},
+                    root_dir = require("lspconfig").util.find_git_ancestor,
+                    init_options = {settings = {args = {}}}
+                }
+            }
+        end
+        if configs.bash_lsp then lspconfig.bash_lsp.setup {} end
+
+        -- enable inlay hints
+        vim.lsp.inlay_hint.enable()
     end
-    if configs.bash_lsp then
-      lspconfig.bash_lsp.setup {}
-    end
-  end
 }
