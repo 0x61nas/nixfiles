83d55b818d98e41387a8d1fd4e2fc7c7bcaa2e9d
0x61nas <anas.elgarhy.dev@gmail.com>
2024-07-24 03:18:04 +0300

    fix(nvim): fix the cursor shape

--- dev/nvim/nvim/lua/plugins/cmp.lua
+++ dev/nvim/nvim/lua/plugins/cmp.lua
@@ -3,11 +3,13 @@ return {
   dependencies = {
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-path',
+    -- 'hrsh7th/cmp-path',
+    "https://codeberg.org/FelipeLema/cmp-async-path",
     'hrsh7th/cmp-cmdline',
     'L3MON4D3/LuaSnip',
     'saadparwaiz1/cmp_luasnip',
   },
+  event = { "LspAttach", "InsertCharPre" },
   config = function()
     local cmp = require('cmp')
     cmp.setup({
@@ -31,20 +33,70 @@ return {
             fallback()
           end
         end, { "i", "s" }),
-      ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-;>'] = cmp.mapping.abort(),
       ['<C-k>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+
+["<c-space>"] = cmp.mapping(function()
+      if not cmp.visible() then
+        cmp.complete()
+      end
+      cmp.abort()
+    end, { "i", "s" }),
+    ["<c-y>"] = cmp.mapping(function(fallback)
+      if not cmp.visible() then
+        fallback()
+      end
+      cmp.scroll_docs(-1)
+    end, { "i", "s" }),
+    ["<c-e>"] = cmp.mapping(function(fallback)
+      if not cmp.visible() then
+        fallback()
+      end
+      cmp.scroll_docs(1)
+    end, { "i", "s" }),
+    ["<c-p>"] = cmp.mapping(function(fallback)
+      if not cmp.visible() then
+        fallback()
+      end
+      cmp.select_prev_item()
+    end, { "i", "s" }),
+    ["<c-n>"] = cmp.mapping(function(fallback)
+      if not cmp.visible() then
+        fallback()
+      end
+      cmp.select_next_item()
+    end, { "i", "s" }),
+    ["<cr>"] = cmp.mapping(function(fallback)
+      if cmp.get_selected_entry() == nil then
+        fallback()
+      end
+      cmp.confirm()
+    end, { "i", "s" }),
     }),
+
     sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
+    { name = "async_path", max_item_count = 20 },
+    {
+      name = "nvim_lsp",
+      max_item_count = 80,
+    },
+    {
+      name = "buffer",
+      max_item_count = 20,
+      option = {
+        get_bufnrs = function()
+          return vim.tbl_map(function(win)
+            return vim.api.nvim_win_get_buf(win)
+          end, vim.api.nvim_list_wins())
+        end,
+      },
+    },
       { name = 'luasnip' }, -- For luasnip users.
       -- { name = 'ultisnips' }, -- For ultisnips users.
       -- { name = 'snippy' }, -- For snippy users.
       -- { name = 'crates' },
-    }, {
-      { name = 'path' },
-      { name = 'buffer' },
     })
+    -- eof
   })
 
   -- Set configuration for specific filetype.
