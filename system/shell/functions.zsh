537950de39f267a125f825618d9c84f547c59810
0x61nas <anas.elgarhy.dev@gmail.com>
2024-07-24 21:39:25 +0300

    feat(zsh): well, am too lazy

---
+++ system/shell/functions.zsh
@@ -0,0 +1,75 @@
+  # Function to check if all arguments are options
+  function all_options() {
+    for arg in "$@"; do
+      if [[ ! $arg == -* ]]; then
+        return 1
+      fi
+    done
+    return 0
+  }
+
+function edit() {
+  if [[ $# -gt 0 && $(all_options "$@") -eq 1 ]]; then
+    $EDITOR $@
+  else
+    # Get the last argument of the previous command
+    prev_arg=${(z)$(fc -ln -1 | awk '{print $NF}')}
+
+    # Check if the last argument is a valid path and a text file
+    if [[ -e $prev_arg && $(file --mime-type -b "$prev_arg") == text/* ]]; then
+      $EDITOR $@ "$prev_arg"
+    else
+      $EDITOR $@
+    fi
+  fi
+}
+
+function gita() {
+    local cmd=(git add)
+  if [[ $# -gt 0 && $(all_options "$@") -eq 1 ]]; then
+    $cmd $@
+  else
+    # Get the last argument of the previous command
+    prev_arg=${(z)$(fc -ln -1 | awk '{print $NF}')}
+
+    if [[ -e $prev_arg ]]; then
+      $cmd $@ "$prev_arg"
+    else
+      $cmd $@
+    fi
+  fi
+}
+
+function gitc() {
+    gita
+    if [[ $# -eq 0 ]]; then
+        git commit --amend
+    elif [[ $# -eq 1 ]]; then
+        git commit -m "$1"
+    elif [[ $# -gt 1 ]]; then
+        local msg=""
+        local files=()
+
+        while [[ $# -gt 0 ]]; do
+            case $1 in
+                -m | --message)
+                    shift
+                    msg="$1"
+                    if [[ -z $msg ]]; then
+                        echo "error: no commit message provided"
+                        break
+                    fi
+                ;;
+                *) files+=("$1");;
+            esac
+            shift
+        done
+
+        gita "${files[@]}"
+        git commit -m "$msg"
+    else
+        echo "ARE YOU AN IDIOT??"
+        type -f gitc
+        return 1
+    fi
+}
