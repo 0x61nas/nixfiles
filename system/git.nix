1379205e9523f2f35f7db11287378792aecb516d
0x61nas <anas.elgarhy.dev@gmail.com>
2026-08-11 18:48:16 +0300

    refactor(git): migrate settings to programs.git.settings and programs.delta

--- system/git.nix
+++ system/git.nix
@@ -1,46 +1,47 @@
 _: {
   programs.git = {
     enable = true;
-    delta.enable = true;
-    userName = "0x61nas";
-    userEmail = "anas.elgarhy.dev@gmail.com";
     ignores = [ "*~" ];
     signing = {
       signByDefault = true;
       key = "0x597AB12E66262898";
     };
-    aliases = {
-      logline = "log --graph --oneline --decorate";
-      # allows you to switch branches quickly. For example, `git co master` instead of `git checkout master`
-      co = "checkout";
-      # simplifying the commit process. For example, `git ci -m "Commit message"` instead of `git commit -m "Commit message"`
-      ci = "commit";
-      # helping you create branches faster. For example, `git br feature` instead of `git branch feature`.
-      br = "branch";
-      st = "status";
-      df = "diff";
-      lg = "log --pretty=\"%C(magenta)%h%Creset %C(yellow)%d%Creset%s %C(bold cyan)(%ar)%Creset\"";
-      lgg = "lg --abbrev-commit --all --graph";
-      unstage = "reset HEAD";
-      last = "log -1 HEAD";
-      undo = "reset HEAD~";
-      pushf = "push --force";
-      squash = "!git rebase -i HEAD~$1";
-      amend = "commit --amend -S";
-      aliases = "config --get-regexp alias";
-      brl = "!git for-each-ref --format='%(refname:short) %(objectname:short)' refs/heads/";
-      uncommit = "reset HEAD^";
-      hist = "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short";
-      ff = "merge --ff-only";
-      shelve = "!git stash && git checkout";
-      unshelve = "stash apply && git stash drop";
-      sync = "!git fetch && git rebase origin/$(git symbolic-ref --short HEAD)";
-      upstream = "branch --set-upstream-to";
-      ignore = "!echo '$1' >> .gitignore";
-      # rebase = "rebase -S";
-      addp = "add --patch";
-    };
-    extraConfig = {
+    settings = {
+      user = {
+        name = "0x61nas";
+        email = "anas.elgarhy.dev@gmail.com";
+      };
+      alias = {
+        logline = "log --graph --oneline --decorate";
+        # allows you to switch branches quickly. For example, `git co master` instead of `git checkout master`
+        co = "checkout";
+        # simplifying the commit process. For example, `git ci -m "Commit message"` instead of `git commit -m "Commit message"`
+        ci = "commit";
+        # helping you create branches faster. For example, `git br feature` instead of `git branch feature`.
+        br = "branch";
+        st = "status";
+        df = "diff";
+        lg = "log --pretty=\"%C(magenta)%h%Creset %C(yellow)%d%Creset%s %C(bold cyan)(%ar)%Creset\"";
+        lgg = "lg --abbrev-commit --all --graph";
+        unstage = "reset HEAD";
+        last = "log -1 HEAD";
+        undo = "reset HEAD~";
+        pushf = "push --force";
+        squash = "!git rebase -i HEAD~$1";
+        amend = "commit --amend -S";
+        aliases = "config --get-regexp alias";
+        brl = "!git for-each-ref --format='%(refname:short) %(objectname:short)' refs/heads/";
+        uncommit = "reset HEAD^";
+        hist = "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short";
+        ff = "merge --ff-only";
+        shelve = "!git stash && git checkout";
+        unshelve = "stash apply && git stash drop";
+        sync = "!git fetch && git rebase origin/$(git symbolic-ref --short HEAD)";
+        upstream = "branch --set-upstream-to";
+        ignore = "!echo '$1' >> .gitignore";
+        # rebase = "rebase -S";
+        addp = "add --patch";
+      };
       init.defaultBranch = "aurora";
       core = {
         eol = "lf";
@@ -49,7 +50,6 @@ _: {
       diff.tool = "nvimdiff";
       difftool.prompt = false;
       difftool."nvimdiff".cmd = "nvim -d \"$LOCAL\" \"$REMOTE\"";
-      # interactive.diffFilter = "delta --color-only";
       github.user = "0x61nas";
       sendemail = {
         from = "anas.elgarhy.dev@gmail.com";
@@ -69,5 +69,9 @@ _: {
         };
       };
     };
+  };
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
   };
 }
