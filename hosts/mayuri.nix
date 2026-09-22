5ea78ec266503ca54a7f127e5124cc82cd088dbe
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2026-09-22 03:22:57 +0300

    feat(mayuri): configure the networking

--- hosts/mayuri.nix
+++ hosts/mayuri.nix
@@ -30,13 +30,13 @@
     enable = true;
   };
 
-  # networking.interfaces.eno1.ipv4.addresses = [{
-  #   address = "192.168.1.100";
-  #   prefixLength = 24;
-  # }];
+  networking.interfaces.enp7s0.ipv4.addresses = [{
+     address = "192.168.1.2";
+     prefixLength = 24;
+  }];
 
-  # networking.defaultGateway = "192.168.1.1";
-  # networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
+  networking.defaultGateway = "192.168.1.1";
+  networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
